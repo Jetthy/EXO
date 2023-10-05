e8f8f1f4cb8dac83d669cef8a8b0ba291f253a5b
GabiGabou <gabriel.boyardPRO@gmail.com>
2023-10-05 20:12:05 +0200

    yeeeee

--- Exo Unity/Assets/Scripts/Exo tkt.cs
+++ Exo Unity/Assets/Scripts/Exo tkt.cs
@@ -3,7 +3,38 @@ using UnityEngine;
 public class Exotkt : MonoBehaviour
 {
     // Start is called before the first frame update
-//uef dghdsjfijdsmù
+    //uef dghdsjfijdsmùoiuqhhgqhjsgfjhqjhsgjh<gqjfghj<qsfb<n;c<llwx<wc
+
+    //LODFadeModes
+    //    d
+    //    private void Start()
+    //{
+    //    LODFadeModesd
+    //        f
+    //        sd
+    //        f
+    //        sdf
+    //        s
+    //        f
+    //        s
+
+    //        r
+    //        g
+    //        f
+    //        f < QualityLevel
+    //        dgq
+    //        sdg
+    //        sd
+    //        g
+    //        sd
+    //        g
+    //        qs
+    //        dg
+    //        qs
+    //        h
+    //        f
+    //        gfdgdgs
+    //}
 
     //}
 
