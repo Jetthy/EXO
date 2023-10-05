418c7acd2dea716e700943a5b77f3c104fa4f11b
Jetthy <cmenana@myges.fr>
2023-10-05 20:10:54 +0200

    tkt la mifa

--- Exo Unity/Assets/Scripts/Exo tkt.cs
+++ Exo Unity/Assets/Scripts/Exo tkt.cs
@@ -3,45 +3,8 @@ using UnityEngine;
 public class Exotkt : MonoBehaviour
 {
     // Start is called before the first frame update
-    //void Start()
-    //{
-    //    lqflbfjkhqsfbjkhsfkgqvnbqkjflkljdjffhjsdhghsjbdvvklshdg,sdkvnklsjddmùqejfndb,nx bc;,vnw: bvqbsdlfjhkqjsbdsjbvhvwsdjhgjhsekjfnb,n dvbsjhdhhfj<nsdfn, b< hideflags; dgfj < bdfnhdfbhb >>
-    //}
-    //webcamdevicer
-    //   wbsee
-    //    bw
-    //    se
-    //    bw
-    //    se
-    //    private void onjointbreak(float breakforce)
-    //{
-    //    waitforsecondsg
-    //        xdd
-    //        h
-    //        x
-    //        gj
-    //        xdf
-    //        h
-    //        d
-    //        gws
-    //        d
-    //        g
-    //        s
-    //        g
-    //        ws
-    //        g
-    //        se
-    //        ggj
-    //        g
-    //        hhk
-    //        g
-    //        hk
-    //        fgh
-    //        kfghkfg
-    //        k
-    //        fghk
-    //        g
-    //        hk
+//uef dghdsjfijdsmù
+
     //}
 
     // Update is called once per frame
