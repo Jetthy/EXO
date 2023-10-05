e78ca04753e2ff8aa11fb0947030f577400cbae9
GabiGabou <gabriel.boyardPRO@gmail.com>
2023-10-05 20:08:20 +0200

    modif gab

--- Exo Unity/Assets/Scripts/Exo tkt.cs
+++ Exo Unity/Assets/Scripts/Exo tkt.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Exotkt : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
+    //    lqflbfjkhqsfbjkhsfkgqvnbqkjflkljdjffhjsdhghsjbdvvklshdg,sdkvnklsjddmùqejfndb,nx bc;,vnw: bvqbsdlfjhkqjsbdsjbvhvwsdjhgjhsekjfnb,n dvbsjhdhhfj<nsdfn, b< hideflags; dgfj < bdfnhdfbhb >>
+    //}
+    //webcamdevicer
+    //   wbsee
+    //    bw
+    //    se
+    //    bw
+    //    se
+    //    private void onjointbreak(float breakforce)
+    //{
+    //    waitforsecondsg
+    //        xdd
+    //        h
+    //        x
+    //        gj
+    //        xdf
+    //        h
+    //        d
+    //        gws
+    //        d
+    //        g
+    //        s
+    //        g
+    //        ws
+    //        g
+    //        se
+    //        ggj
+    //        g
+    //        hhk
+    //        g
+    //        hk
+    //        fgh
+    //        kfghkfg
+    //        k
+    //        fghk
+    //        g
+    //        hk
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
